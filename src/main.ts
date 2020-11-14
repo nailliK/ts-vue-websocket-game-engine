3271e6a745112dca0fda73edd2ca5151771d1dc7
Killian Louis Grant <killian@killiangrant.com>
2020-11-14 11:35:56 -0700

    bump NPM versions, add open-iconic icon set

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import {createApp} from "vue";
-import App from "./App.vue";
 import "./registerServiceWorker";
+import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import {createApp} from "vue";
 
 const app = createApp(App);
 app.use(store);
